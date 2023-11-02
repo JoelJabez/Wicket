e17d063caee293126064a3e7b0d1653e44f668a9
joeljabez0134 <joeldavethu@gmail.com>
2023-11-02 17:36:53 +0800

    learned about wicket

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -5,17 +5,8 @@ import org.apache.wicket.csp.CSPDirectiveSrcValue;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
- * 
- * @see com.example.Start#main(String[])
- */
 public class WicketApplication extends WebApplication
 {
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
 	@Override
 	public Class<? extends WebPage> getHomePage()
 	{
